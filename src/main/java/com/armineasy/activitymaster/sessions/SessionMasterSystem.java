026f62779295428afbf324d423f79b0218f6f984
GedMarc <ged_marc@hotmail.com>
2019-07-09 23:48:40 +0200

    Module Encapsulation

--- src/main/java/com/armineasy/activitymaster/sessions/SessionMasterSystem.java
+++ src/main/java/com/armineasy/activitymaster/sessions/SessionMasterSystem.java
@@ -1,15 +1,12 @@
 package com.armineasy.activitymaster.sessions;
 
-import com.armineasy.activitymaster.activitymaster.implementations.ClassificationService;
-import com.armineasy.activitymaster.activitymaster.implementations.SystemsService;
 import com.armineasy.activitymaster.activitymaster.services.IActivityMasterProgressMonitor;
 import com.armineasy.activitymaster.activitymaster.services.IActivityMasterSystem;
 import com.armineasy.activitymaster.activitymaster.services.dto.IEnterprise;
 import com.armineasy.activitymaster.activitymaster.services.dto.ISystems;
-import com.armineasy.activitymaster.activitymaster.services.system.IResourceItemService;
-import com.armineasy.activitymaster.activitymaster.systems.SystemsSystem;
+import com.armineasy.activitymaster.activitymaster.services.system.IClassificationService;
+import com.armineasy.activitymaster.activitymaster.services.system.ISystemsService;
 import com.google.inject.Singleton;
-import com.jwebmp.guicedinjection.GuiceContext;
 import com.jwebmp.guicedinjection.interfaces.JobService;
 import com.jwebmp.logger.LogFactory;
 
@@ -18,7 +15,6 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-import static com.armineasy.activitymaster.profiles.enumerations.ProfileClassifications.*;
 import static com.armineasy.activitymaster.sessions.services.classifications.SessionClassifications.*;
 import static com.jwebmp.guicedinjection.GuiceContext.*;
 
@@ -55,7 +51,7 @@ public class SessionMasterSystem
 
 	private void done(IEnterprise<?> enterprise)
 	{
-		ClassificationService classificationService = get(ClassificationService.class);
+		IClassificationService<?> classificationService = get(IClassificationService.class);
 
 		classificationService.create(SessionClassifications, newSystem.get(enterprise));
 		classificationService.create(SessionObject, newSystem.get(enterprise));
@@ -64,11 +60,11 @@ public class SessionMasterSystem
 	@Override
 	public void postUpdate(IEnterprise<?> enterprise, IActivityMasterProgressMonitor progressMonitor)
 	{
-		newSystem.put(enterprise, get(SystemsService.class)
-		                                      .create(enterprise, "Sessions Master",
-		                                              "The system for handling distributed sessions", ""));
-		uuid = get(SystemsSystem.class)
-		                   .registerNewSystem(enterprise, newSystem.get(enterprise));
+		newSystem.put(enterprise, get(ISystemsService.class)
+				                          .create(enterprise, "Sessions Master",
+				                                  "The system for handling distributed sessions", ""));
+		uuid = get(ISystemsService.class)
+				       .registerNewSystem(enterprise, newSystem.get(enterprise));
 
 		LogFactory.getLog("SessionMaster")
 		          .warning("Waiting for all systems to generate their security identities");
