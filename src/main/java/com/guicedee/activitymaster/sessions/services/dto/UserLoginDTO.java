0ef48145ff57a5361d9bfa656477efdbc0ddbef7
GedMarc <marc.magon@gmail.com>
2021-04-20 03:26:33 +0200

    Events updates and JMod builds

--- src/main/java/com/guicedee/activitymaster/sessions/services/dto/UserLoginDTO.java
+++ src/main/java/com/guicedee/activitymaster/sessions/services/dto/UserLoginDTO.java
@@ -1,6 +1,8 @@
 package com.guicedee.activitymaster.sessions.services.dto;
 
 import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.NullSerializer;
 import com.guicedee.activitymaster.profiles.dto.ProfileServiceDTO;
 import lombok.*;
 import lombok.experimental.Accessors;
@@ -11,8 +13,8 @@ import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.*;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonAutoDetect(fieldVisibility = ANY,getterVisibility = NONE,setterVisibility = NONE)
-@EqualsAndHashCode(of = {"userName"},callSuper = false)
+@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, setterVisibility = NONE)
+@EqualsAndHashCode(of = {"userName"}, callSuper = false)
 @Getter
 @Setter
 @Accessors(chain = true)
@@ -20,7 +22,9 @@ public class UserLoginDTO<J extends UserLoginDTO<J>>
 		extends ProfileServiceDTO<J>
 		implements Serializable
 {
+	@JsonSerialize(using = NullSerializer.class)
 	private String userName;
+	@JsonSerialize(using = NullSerializer.class)
 	private String password;
 	private boolean rememberMe;
 }
